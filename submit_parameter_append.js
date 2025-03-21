39cfe476af44971dbaa13fed8c701015a0a8656f
Mohsendirbaz <dirbaz.sharif@gmail.com>
2025-03-20 19:36:29 -0500

    Update # node path corrected

--- submit_parameter_append.js
+++ submit_parameter_append.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.text({ type: '*/*' })); // Use text parser for all content types
 app.use(cors());
 
-const NF3_ML_DIR = "C:\\Users\\Mohse\\OneDrive\\Desktop\\Milestone4 - Copy";
+const NF3_ML_DIR = path.join(__dirname);
 
 // Function to check write permissions
 const checkWritePermissions = (dir) => {
